cb0300f8efa09fe235ec3d3569809764f058f269
Deksu <minxzu@gmail.com>
2018-04-17 00:01:43 +0300

    fixed typo on title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Simple Gif Searcher',
+    title: 'Simple GIF Searcher',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
